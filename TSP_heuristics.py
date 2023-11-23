1f8d8444be797db50e2885770fbdd4a07bc3ad08
julienpillis <pllsjulien@gmail.com>
2023-11-23 22:40:12 +0100

    ajout heuristique de Fletcher (TSP)

--- TSP_heuristics.py
+++ TSP_heuristics.py
@@ -132,3 +132,109 @@ def best_insertion(points, starting_point, ending_point):
         pts.pop(0)
 
     return tour
+
+def fletcher(points, starting_point, ending_point):
+    pts = [point for point in points]
+
+    pts.pop(pts.index(starting_point))  # suppression du départ
+    pts.pop(pts.index(ending_point))  # suppression de l'arrivée
+
+    times = utils.times_calculator(pts)
+    edges = []
+    times = sorted(times.items(), key=lambda t: t[1]) # Tri par ordre croissant des distances
+    times = [time[0] for time in times]
+    while len(times) > 0:
+        used_nodes = extract_inner_tuples(edges)
+        if times[0][0] not in used_nodes and times [0][1] not in used_nodes :
+            # Si aucun noeud est extrémité d'une arête, on peut les ajouter sans problème
+            edges.append((times[0][0],times[0][1]))
+        elif times[0][0] in used_nodes and times [0][1] not in used_nodes:
+            if used_nodes.count(times[0][0]) >= 2 :
+                # Création d'une fourche si ajout d'un arc !
+                pass
+            else :
+                edges.append((times[0][0], times[0][1]))
+        elif times[0][1] in used_nodes and times [0][0] not in used_nodes:
+            if used_nodes.count(times[0][1]) >= 2:
+                # Création d'une fourche si ajout d'un arc !
+                pass
+            else:
+                edges.append((times[0][0],times[0][1]))
+        else :
+            # Si les 2 noeuds sont déjà ajoutés
+            if is_cycle(edges + [(times[0][0],times[0][1])]):
+                # Si l'ajout de l'arête forme un cycle, on ne l'ajoute pas:
+                pass
+            elif used_nodes.count(times[0][0]) >= 2 or used_nodes.count(times[0][1]) >= 2:
+                # Ajouter l'arc formerait une fourche !
+                pass
+            else :
+                edges.append((times[0][0],times[0][1]))
+        times.pop(0)
+
+
+    # Réordonnancement du tableau
+    no_match = [node for node in extract_inner_tuples(edges) if extract_inner_tuples(edges).count(node)==1] # Récupération des noeuds n'ayant pas 2 arêtes
+    ordered_edges = [(starting_point,no_match[0])]
+    while len(edges) > 0:
+        match = [edge for edge in edges if edge[0]==ordered_edges[-1][1] or edge[1]==ordered_edges[-1][1]] # Récupération de l'arête ayant le même sommet que le 2ème noeud de la dernière arête
+        if match[0][0]==ordered_edges[-1][1]:                                                              # S'il se trouve en première place du tuple, on l'ajoute
+            ordered_edges.append(match[0])
+        else :
+            ordered_edges.append((match[0][1],match[0][0]))                                                 # Sinon, on inverse le tuple
+        edges.remove(match[0])
+    ordered_edges.append((ordered_edges[-1][1],ending_point))
+    return [edge[0] for edge in ordered_edges]+[ending_point]
+
+
+
+def extract_inner_tuples(array_of_tuples):
+    """Extraire les tuples d'une liste de tuples"""
+    inner_tuples = []
+    for outer_tuple in array_of_tuples:
+        for inner_tuple in outer_tuple:
+            inner_tuples.append(inner_tuple)
+    return inner_tuples
+
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [0] * size
+
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i, j):
+        root_i = self.find(i)
+        root_j = self.find(j)
+
+        if root_i != root_j:
+            if self.rank[root_i] > self.rank[root_j]:
+                self.parent[root_j] = root_i
+            elif self.rank[root_i] < self.rank[root_j]:
+                self.parent[root_i] = root_j
+            else:
+                self.parent[root_j] = root_i
+                self.rank[root_i] += 1
+
+def is_cycle(graph):
+    coord_to_index = {}  # Dictionnaire pour mapper les coordonnées aux indices
+    index = 0
+
+    for edge in graph:
+        for node in edge:
+            if node not in coord_to_index:
+                coord_to_index[node] = index
+                index += 1
+
+    uf = UnionFind(len(coord_to_index))
+
+    for edge in graph:
+        u, v = map(coord_to_index.get, edge)
+        if uf.find(u) == uf.find(v):
+            return True
+        uf.union(u, v)
+
+    return False
