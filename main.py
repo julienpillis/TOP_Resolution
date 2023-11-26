9745afee4dad5ab3f24086a3f24b1f3fb5308a1d
julienpillis <pllsjulien@gmail.com>
2023-11-26 09:37:26 +0100

    ajout starting point and ending point

--- main.py
+++ main.py
@@ -4,25 +4,13 @@ import time
 from TSP_heuristics import *
 
 if __name__ == "__main__":
-    for c in list(map(chr, range(ord('b'), ord('r') + 1))):
+    for c in list(map(chr, range(ord('a'), ord('r') + 1))):
         # Exemple d'utilisation
-        file_path = f"Set_32_234/p1.4.{c}.txt"
+        file_path = f"Set_100_234/p4.3.{c}.txt"
         print(file_path)
         graph_object = read_file_and_create_graph(file_path)
 
-        # Accéder aux données
-        # print("Nodes:", graph_object.nodes)
-        # print("Times:", graph_object.times)
-        # print("Profits:", graph_object.profits)
-        # print("Tmax", graph_object.getMaxTime())
-        # print("NbVehicules:", graph_object.getNbVehicules())
-        # print("NbNodes",  len(graph_object.nodes))
-
-        # Exemple d'utilisation
-        # points = [(0, 0), (1, 2), (2, 4), (3, 1), (4, 3)]
-        # profits ={(0,0) : 0,(1,2) :0,(2,4) : 2, (3,1) : 3,(4,3) : 2}
-        # graph = Graph(points,times_calculator(points),profits)
-
+        print("Time MAX path :",graph_object.maxTime)
         # Temps de départ
         temps_debut = time.time()
 
@@ -37,4 +25,5 @@ if __name__ == "__main__":
         temps_execution = temps_fin - temps_debut
         print("==============================")
 
-        visualize_paths(graph_object.nodes, [path for path in convoy], profit, temps_execution)
+        if convoy:
+            visualize_paths(graph_object.nodes, [path for path in convoy],convoy[0][0],convoy[0][-1],profit, temps_execution)
