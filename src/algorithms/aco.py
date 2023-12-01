bced7e5e8f163339d562852bbb443c61b87c9e17
julienpillis <pllsjulien@gmail.com>
2023-12-01 20:49:43 +0100

    améliorations beasley.py

--- src/algorithms/aco.py
+++ src/algorithms/aco.py
@@ -13,13 +13,13 @@ def ant_colony_optimization(graph : utils.Graph,
                             starting_point : int,
                             ending_point : int,
                             n_ants : int = 20 ,
-                            n_iterations : int = 100,
-                            Ncycles : int = 2,
+                            n_iterations : int = 10,
+                            Ncycles : int = 20,
                             alpha : float = 1,
                             beta : float = 0.5,
                             gamma : float = 0.5,
                             evaporation_rate : float = 0.98,
-                            Pbest: float =0.5) -> tuple[Path, Profit]:
+                            Pbest: float =0.05) -> tuple[Path, Profit]:
     """
     Meta-heuristique Ant colony optimization adaptée au problème du TOP
 
@@ -35,98 +35,113 @@ def ant_colony_optimization(graph : utils.Graph,
     :returns : les meilleurs chemins et le profit total
     """
 
-
-    points = graph.nodes
-    n_points = len(points)
-    best_paths = []
-    used_nodes = []                     # Nœuds inaccessibles ou appartenant déjà à une tournée
+    n_points = len(graph.nodes)
+    no_visit = []                     # Nœuds inaccessibles ou appartenant déjà à une tournée
 
     # ======================= Optimisation calculatoire ======================#
     # On supprime les noeuds inatteignables
-    for point in points :
+    for point in graph.nodes :
         if point not in [graph.nodes[starting_point],graph.nodes[ending_point]] and graph.times[(graph.nodes[starting_point],point)] + graph.times[(point,graph.nodes[ending_point])] > graph.maxTime :
-            used_nodes.append(graph.nodes.index(point))
+            no_visit.append(graph.nodes.index(point))
     # ========================================================================#
 
-    for route in range(graph.nbVehicules):
-        sib = []  # iteration best solution
-        sgb = []  # global best solution
-        Nni = 0
-        pheromone = np.ones((n_points, n_points))
-        for iteration in range(n_iterations):
-            paths = []
-            norm_paths = []
-            n_points = len(graph.nodes)
-            for ant in range(n_ants):
+    sib = []  # iteration best solution
+    sgb = []  # global best solution
+    Nni = 0
+    pheromone = np.ones((n_points, n_points))
+    for iteration in range(n_iterations):
+        print(iteration)
+        std_sol = [] # Solution au format standard (liste de tuples)
+        for ant in range(n_ants):
 
-                # ======================= Construct Solution =============================#
-                path = constructSolution(graph ,starting_point, ending_point, used_nodes ,pheromone ,alpha, gamma, beta)
-                # ========================================================================#
-
-                #======================= Local Search ====================================#
-                path = localS.two_opt([graph.nodes[i] for i in path], graph.maxTime, graph.profits, graph.times, graph.nodes,[graph.nodes[i] for i in path] + [graph.nodes[i] for i in used_nodes])
-                # ========================================================================#
-                norm_paths.append(path)
-                path = [graph.nodes.index(node) for node in path]
-                paths.append(path)
-
-
-            sib = paths[np.argmax([F(graph,[graph.nodes[i] for i in path]) for path in paths])]
-            if F(graph,[graph.nodes[i] for i in sib]) > F(graph,[graph.nodes[i] for i in sgb]):
-                sgb = sib
-                Nni = 0
-            else :
-                Nni += 1
-
-            #======================= Pheromone update ================================#
-            pheromoneUpdate(graph, pheromone, sgb, sib, evaporation_rate, Pbest, Nni, Ncycles, iteration)
+            # ======================= Construction d'une solution =============================#
+            sol_ant = constructSolution(graph ,no_visit,starting_point, ending_point, pheromone ,alpha, gamma, beta)
             # ========================================================================#
 
-        for i in sgb:
-            used_nodes.append(i)
-        best_paths.append([graph.nodes[i] for i in sgb])
-    return best_paths, sum([utils.calculate_profit(best_paths[i], graph.profits) for i in range(graph.nbVehicules)])
+            #======================= Recherche Locale ====================================#
+            used_nodes = []
+            for path in sol_ant:
+                for node in path[1:-1]:
+                    used_nodes.append(node)
+            for i in range(len(sol_ant)):
+                sol_ant[i] = localS.two_opt([graph.nodes[i] for i in sol_ant[i]], graph.maxTime, graph.profits, graph.times, graph.nodes,[graph.nodes[i] for i in used_nodes+no_visit])
+            # ========================================================================#
+            std_sol.append(sol_ant)
+
+        #======================= Recherche de la meilleure solution trouvée par les fourmis ================================#
+        sib = []
+        max_quality = 0
+        for solution in std_sol:
+            quality = F(graph,solution)
+            if quality > max_quality :
+                max_quality = quality
+                sib = solution
+        # ==================================================================================================================#
+
+        # ======================= Mise à jour de la meilleure solution ================================#
+        if F(graph,sib) > F(graph,sgb):
+            sgb = sib
+            Nni = 0
+        else :
+            Nni += 1
+
+        # ==================================================================================================================#
+
+        #======================= Mise à jour des phéromones ================================#
+        pheromoneUpdate(graph, pheromone, sgb, sib, evaporation_rate, Pbest, Nni, Ncycles, iteration)
+        #========================================================================#
 
 
-def constructSolution(graph, starting_point, ending_point, used_nodes ,pheromone ,alpha, gamma, beta):
-    """Construction d'une tournée."""
+    return sgb, sum(utils.calculate_profit(path,graph.profits) for path in sgb)
+
+
+def constructSolution(graph,no_visit, starting_point, ending_point,pheromone ,alpha, gamma, beta):
+    """Construction d'une tournée. Méthode séquentielle """
     n_points = len(graph.nodes)
+    solution = []
+    used_nodes = [node for node in no_visit]
+    for _ in range(graph.nbVehicules):
+        visited = [False] * n_points
+        for i in used_nodes:
+            visited[i] = True
+        current_point = starting_point
+        visited[current_point] = True
+        visited[ending_point] = True
+        path = [current_point, ending_point]
+        while False in visited:
+            tmp_path = [i for i in path]
+            unvisited = np.where(np.logical_not(visited))[0]
+            probabilities = np.zeros(len(unvisited))
 
-    # On signale
-    visited = [False] * n_points
-    for i in used_nodes:
-        visited[i] = True
-    current_point = starting_point
-    visited[current_point] = True
-    visited[ending_point] = True
-    path = [current_point, ending_point]
-    while False in visited:
-        tmp_path = [i for i in path]
-        unvisited = np.where(np.logical_not(visited))[0]
-        probabilities = np.zeros(len(unvisited))
+            for i, unvisited_point in enumerate(unvisited):
+                # Calcul des probabilités de chaque noeud
+                rj = graph.profits[graph.nodes[unvisited_point]]
+                cij = graph.times[(graph.nodes[current_point], graph.nodes[unvisited_point])]
+                cin = graph.times[(graph.nodes[current_point], graph.nodes[path[-1]])]
+                cjn = graph.times[(graph.nodes[unvisited_point], graph.nodes[path[-1]])]
+                teta_ij = (cij ** 2 + cin ** 2 - cjn ** 2) / (2 * cij * cin)
+                neta = (rj / cij) * math.exp(teta_ij * gamma)
 
-        for i, unvisited_point in enumerate(unvisited):
-            # Calcul des probabilités de chaque noeud
-            rj = graph.profits[graph.nodes[unvisited_point]]
-            cij = graph.times[(graph.nodes[current_point], graph.nodes[unvisited_point])]
-            cin = graph.times[(graph.nodes[current_point], graph.nodes[path[-1]])]
-            cjn = graph.times[(graph.nodes[unvisited_point], graph.nodes[path[-1]])]
-            teta_ij = (cij ** 2 + cin ** 2 - cjn ** 2) / (2 * cij * cin)
-            neta = (rj / cij) * math.exp(teta_ij * gamma)
-            probabilities[i] = (pheromone[current_point, unvisited_point] ** alpha) * (neta ** beta)
-        probabilities /= np.sum(probabilities)
+                probabilities[i] = (pheromone[current_point, unvisited_point] ** alpha) * (neta ** beta)
+            probabilities /= np.sum(probabilities)
 
-        # On détermine le prochain point
-        next_point = np.random.choice(unvisited, p=probabilities)
+            # On détermine le prochain point
+            next_point = np.random.choice(unvisited, p=probabilities)
 
-        tmp_path.insert(-1, next_point)
-        visited[next_point] = True
-        current_point = next_point
-        if utils.calculate_time([graph.nodes[i] for i in tmp_path], graph.times) < graph.maxTime:
-            # On insère après le dernier sommet ajouté (pas en dernière position car il s'agit du sommet de destination)
-            path.insert(-1, next_point)
+            tmp_path.insert(-1, next_point)
+            visited[next_point] = True
+            current_point = next_point
+            if utils.calculate_time([graph.nodes[i] for i in tmp_path], graph.times) < graph.maxTime:
+                # On insère après le dernier sommet ajouté (pas en dernière position car il s'agit du sommet de destination)
+                path.insert(-1, next_point)
 
-    return path
+        # On signale que les sommets du chemin produit ne sont plus disponible
+        for node in path :
+            used_nodes.append(node)
+
+        solution.append(path)
+
+    return solution
 
 
 def pheromoneUpdate(graph,pheromone,sgb,sib,evaporation_rate,Pbest,Nni,Ncycles,iteration):
@@ -134,28 +149,31 @@ def pheromoneUpdate(graph,pheromone,sgb,sib,evaporation_rate,Pbest,Nni,Ncycles,i
     n_points = len(graph.nodes)
 
     # Valeur maximale des phéromones sur un noeud
-    t_max = F(graph, [graph.nodes[i] for i in sgb]) / (1 - evaporation_rate)
+    t_max = F(graph, sgb) / (1 - evaporation_rate)
 
     # Valeur minimale des phéromones sur un noeud
-    t_min = (1 - (Pbest ** (1 / n_points))) / (((n_points / 2) - 1) * (Pbest ** (1 / n_points))) * t_max
+    t_min = (1 - (Pbest ** (1 / n_points))) / (((n_points / 2) - 1) * (Pbest ** (1 / n_points))* t_max)
 
     if Nni == Ncycles:
         # Renouvellement des phéromones tout les Ncycles
         pheromone.fill(t_max)
     else:
         if iteration % 5 == 0:
-            path = sgb
+            sol = sgb
         else:
-            path = sib
+            sol = sib
+        used_nodes = []
+        for path in sol :
+            used_nodes += utils.extract_inner_tuples(path)
         for i in range(n_points - 1):
             for j in range(n_points):
-                pheromone[i][j] = pheromone[i][j] * (1 - evaporation_rate) + F(graph,[graph.nodes[i] for i in path]) if (i in path and path[path.index(i) + 1] == j) else 0
+                pheromone[i][j] = pheromone[i][j] * (1 - evaporation_rate) + F(graph,sol) if (i,j) in used_nodes else 0
                 if pheromone[i][j] < t_min:
                     pheromone[i][j] = t_min
                 elif pheromone[i][j] > t_max:
                     pheromone[i][j] = t_max
 
 
-def F(graph : utils.Graph,path : Path):
+def F(graph : utils.Graph,sol : list[Path]):
     """Fonction de qualité : permet d'évaluer la qualité d'un chemin"""
-    return (utils.calculate_profit(path,graph.profits))/(sum(graph.profits[node] for node in graph.nodes))
+    return sum(utils.calculate_profit(path[1:-1],graph.profits) for path in sol)/(sum(graph.profits[node] for node in graph.nodes[1:-1]))
